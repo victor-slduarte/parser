90a65c6335fe79aa086e37b179b30ed879f02d04
Bruno Passos <bgpassos@gmail.com>
2018-11-14 20:23:03 -0200

    Adding users show

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get '/test' => redirect("http://192.168.15.170/"), :as => :test
 
   resources :angels
+
+  get 'users/:id' => 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
