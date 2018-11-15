d12d0d5f59839eb1d17469e7a60ec2b84fe8f2a6
Victor Duarte <victor.slduarte@gmail.com>
2018-11-15 12:40:30 -0200

    ola

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
 
-def show
-  @user = User.find(params[:id])
-end
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def dashboard
+    @angels = Angels.new
+  end
+
+  def map
+  end
 
 end
