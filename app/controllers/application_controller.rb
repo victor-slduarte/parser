6b2214f583b198c90bfd2cbda720480aeeae4fa4
Victor Duarte <victor.slduarte@gmail.com>
2018-11-15 16:42:13 -0200

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :last_name, :address, :phone_number, :email, :login, :password, :password_confirmation, :remember_me])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :last_name, :address, :phone_number, :email, :login, :car_model, :car_plate, :password, :password_confirmation, :remember_me])
     devise_parameter_sanitizer.permit(:sign_in, keys: [ :login, :password])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :email, :first_name, :last_name, :address, :phone_number, :car_model, :car_plate])
   end
