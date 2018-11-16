d55a30f4fec0562e6ecf668438bd8fd3cb61973e
Stephanie Shahini <stelili@hotmail.com>
2018-11-16 17:13:01 -0200

    dashboard created with paths

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
 
+puts "seeding angels..."
+
+100.times do
+  a = Angel.new(
+    first_name: Faker::Cannabis.cannabinoid,
+    last_name: Faker::Cannabis.strain ,
+    phone_number: rand(10000000),
+    user_id: 1
+    )
+  a.save!
+end
+
+puts "seed complete!!! :)"
