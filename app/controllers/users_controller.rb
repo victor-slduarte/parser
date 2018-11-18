bb0c1f9bebb28ac750495cccde1775d1c675aefd
Bruno Passos <bgpassos@gmail.com>
2018-11-18 11:15:10 -0200

    We have latitude and longitude

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[danger show]
   before_action :set_user, only: %i[show danger]
-
+  require 'open-uri'
 
   def show
   end
@@ -13,6 +13,11 @@ class UsersController < ApplicationController
 
   def map
     @user = current_user
+    url = "https://api.thingspeak.com/channels/630025/feeds.json?api_key=O082B0T51Z0IM9UP&results=2"
+    serialized_location = open(url).read
+    hash = JSON.parse(serialized_location)
+    @latitude = hash["feeds"][1]["field1"]
+    @longitude = hash["feeds"][1]["field2"]
   end
 
   private
