7d9934ed6ccb4c73dbedbb0e94f951725e29d061
Victor Duarte <victor.slduarte@gmail.com>
2018-11-18 22:42:58 -0200

    resolvi o bug de login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource_or_scope)
@@ -14,6 +14,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :last_name, :address, :phone_number, :car_model, :car_plate, :password, :password_confirmation, :remember_me])
     devise_parameter_sanitizer.permit(:account_update, keys: [:email, :first_name, :last_name, :address, :phone_number, :car_model, :car_plate])
   end
-
 
 end
