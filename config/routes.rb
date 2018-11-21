d46bc19ddfdfea10113e3a7f99397b17260393da
Victor Duarte <victor.slduarte@gmail.com>
2018-11-21 12:28:47 -0200

    conflict on routs solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  devise_for :users, :controllers => { :registrations => :registrations }
-=======
+
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
->>>>>>> 70762c513ecfbb12a08d83389026a993ebb2a255
 
   root to: 'pages#home'
 
